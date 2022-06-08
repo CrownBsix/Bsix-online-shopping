019026f609215a848f27494d948c3abcbb90f2b6
CrownNdlala <CrownNdlala@LAPTOP-BTI1M5QP>
2022-06-08 08:27:17 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bsix_online_shopping.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
